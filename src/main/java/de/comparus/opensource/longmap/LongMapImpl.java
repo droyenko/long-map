2c1a2cbd593b6e60a5ee5b500b9464a0a76e52f8
Dmytro Roienko <droie@softserveinc.com>
2018-10-25 00:47:45 +0300

    implementation of long-map with 100% code coverage

--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -1,43 +1,130 @@
 package de.comparus.opensource.longmap;
 
+import java.util.Arrays;
+
 public class LongMapImpl<V> implements LongMap<V> {
+    private int size;
+    private int DEFAULT_CAPACITY = 16;
+    private Entry<V>[] values = new Entry[DEFAULT_CAPACITY];
+
+    static class Entry<V> {
+        private final long key;
+        private V value;
+
+        Entry(long key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        long getKey() {
+            return key;
+        }
+
+        V getValue() {
+            return value;
+        }
+
+        void setValue(V value) {
+            this.value = value;
+        }
+    }
+
     public V put(long key, V value) {
-        return null;
+        boolean insert = true;
+        for (int i = 0; i < size; i++) {
+            if (values[i].getKey() == key) {
+                values[i].setValue(value);
+                insert = false;
+            }
+        }
+        if (insert) {
+            ensureCapacity();
+            values[size++] = new Entry<>(key, value);
+        }
+        return value;
+    }
+
+    private void ensureCapacity() {
+        if (size == values.length) {
+            int newSize = values.length * 2;
+            values = Arrays.copyOf(values, newSize);
+        }
     }
 
     public V get(long key) {
+        for (int i = 0; i < size; i++) {
+            if (values[i] != null) {
+                if (values[i].getKey() == key) {
+                    return values[i].getValue();
+                }
+            }
+        }
         return null;
     }
 
     public V remove(long key) {
-        return null;
+        V value = null;
+        for (int i = 0; i < size; i++) {
+            if (values[i].getKey() == key) {
+                value = values[i].getValue();
+                values[i] = null;
+                size--;
+                compressArray(i);
+            }
+        }
+        return value;
+    }
+
+    private void compressArray(int start) {
+        for (int i = start; i < size; i++) {
+            values[i] = values[i + 1];
+        }
     }
 
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public boolean containsKey(long key) {
+        for (int i = 0; i < size; i++) {
+            if (values[i].getKey() == key) {
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean containsValue(V value) {
+        for (int i = 0; i < size; i++) {
+            if (values[i].getValue().equals(value)) {
+                return true;
+            }
+        }
         return false;
     }
 
     public long[] keys() {
-        return null;
+        long[] result = new long[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = values[i].getKey();
+        }
+        return result;
     }
 
     public V[] values() {
-        return null;
+        V[] result = (V[]) new Object[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = values[i].getValue();
+        }
+        return result;
     }
 
     public long size() {
-        return 0;
+        return size;
     }
 
     public void clear() {
-
+        size = 0;
+        Arrays.fill(values, null);
     }
 }
---
+++ src/test/java/de.comparus.opensource.longmap/LongMapImplTest.java
@@ -0,0 +1,73 @@
+package de.comparus.opensource.longmap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.*;
+
+public class LongMapImplTest {
+
+    private LongMap map = new LongMapImpl();
+
+    @Before
+    public void setUp() {
+        IntStream.range(1,101).forEach(i -> map.put(i, "value_" + i ));
+    }
+
+    @Test
+    public void testPut() {
+        assertEquals(100, map.size());
+        map.put(101, "value_101");
+        assertEquals(101, map.size());
+        map.put(50, "duplicate");
+        assertEquals(101, map.size());
+        assertEquals("duplicate", map.get(50));
+    }
+
+    @Test
+    public void testGet() {
+        assertEquals("value_44", map.get(44));
+        assertNull(map.get(200));
+    }
+
+    @Test
+    public void testRemove() {
+        assertEquals("value_50", map.remove(50));
+        assertEquals(99, map.size());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertFalse(map.isEmpty());
+        map.clear();
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void testContainsKey() {
+        assertTrue(map.containsKey(100));
+        assertFalse(map.containsKey(101));
+    }
+
+    @Test
+    public void testContainsValue() {
+        assertTrue(map.containsValue("value_100"));
+        assertFalse(map.containsValue("value_101"));
+    }
+
+    @Test
+    public void testKeys() {
+        long[] expected = new long[100];
+        IntStream.range(1,101).forEach(i -> expected[i-1]=i);
+        assertArrayEquals(expected, map.keys());
+    }
+
+    @Test
+    public void testValues() {
+        String[] expected = new String[100];
+        IntStream.range(1,101).forEach(i -> expected[i-1]="value_" + i);
+        assertArrayEquals(expected, map.values());
+    }
+}
